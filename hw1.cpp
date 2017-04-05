830455870682cc7eb0e40ef108b55bcded6f2673
ethanrblank <ethanrb@gmail.com>
2017-04-05 14:46:03 -0700

    Add files via upload

---
+++ hw1.cpp
@@ -0,0 +1,42 @@
+/*
+ Ethan Blank                        PIC 10A Intro. to Programming
+ ID: 204381410                      Spring 2016
+ Email: ethanrb@gmail.com           Homework #1
+ Section: 2B
+ Honesty Pledge:
+ 
+ I, Ethan Blank, pledge that this is my own independent
+ work, which conforms to the guidlines of academic honesty as
+ described in the course syllabus.
+ 
+ List of known bugs: None.
+ */
+
+#include <iostream>
+
+using namespace std;
+ 
+int main() {
+    cout <<        "123456789123456789123456789123456789123456789       \n\n\n";
+    cout <<        "              ___  _____                                \n";
+    cout <<        "            .'/,-Y\"     \"~-.                          \n";
+    cout <<        "            l.Y             ^.                          \n";
+    cout <<        "            /\\               _\\_                      \n";
+    cout <<        "           i            ___/\"   \"\\                   \n";
+    cout <<        "           :          /\"   \"\\  o  !                  \n";
+    cout <<        "           l         ]     o !__./                      \n";
+    cout <<        "            \\ _  _    \\.___./   \"~\\                 \n";
+    cout <<        "             X \\/  \\          ___./                   \n";
+    cout <<        "            \( \\ ___.   _..--~~\"   ~`_.      _____    \n";
+    cout <<        "             ` Z,--   /               \\    /     \\    \n";
+    cout <<        "               \\__.  \(   /       ______)  < D'oh!|    \n";
+    cout <<        "                 \\   l  /-----~~\" /        \\_____/   \n";
+    cout <<        "                  Y   \\          /                     \n";
+    cout <<        "                  :    \"x______.^                      \n";
+    cout <<        "                  :           \\                        \n";
+    cout <<        "                  j            Y                  \n\n\n\n";
+    cout <<        "123456789123456789123456789123456789123456789" << endl;
+
+system("pause");
+return 0;
+}
